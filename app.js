d5409451963243ae04c19179cad5ef843e9fad5a
phillia-m <philliashaie@gmail.com>
2022-03-22 14:48:28 +0200

    main

---
+++ app.js
@@ -0,0 +1,8 @@
+const hour = document.getElementById('hour');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time() {
+    
+}
+)
